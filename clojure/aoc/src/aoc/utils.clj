62feec0487bc3cc3ab52cb98c5739e0807a26b07
Ossi Pettersson <ossi@moosh.dev>
2022-12-04 13:37:58 +0200

    refactoring input files

--- clojure/aoc/src/aoc/utils.clj
+++ clojure/aoc/src/aoc/utils.clj
@@ -1,4 +1,4 @@
 (ns aoc.utils)
 
 (defn read-input [day & {:keys [sample?] :or {sample? false}}]
-  (slurp (str (System/getProperty "user.dir") "/input/" day (when sample? "_sample") ".txt")))
+  (slurp (str (System/getProperty "user.dir") "/../../inputs/" day (when sample? "_sample") ".txt")))
