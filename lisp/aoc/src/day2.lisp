fb81e6dac56960beb12d6995d22b9b950b1bf0fa
Ossi <ossi@moosh.dev>
2022-12-05 07:34:11 +0200

    day2: common lisp

---
+++ lisp/aoc/src/day2.lisp
@@ -0,0 +1,22 @@
+(in-package #:aoc)
+
+(defparameter *shape-scores*
+  '(("A X" . 4) ("A Y" . 8) ("A Z" . 3)
+    ("B X" . 1) ("B Y" . 5) ("B Z" . 9)
+    ("C X" . 7) ("C Y" . 2) ("C Z" . 6)))
+
+(defparameter *outcome-scores*
+  '(("A X" . 3) ("A Y" . 4) ("A Z" . 8)
+    ("B X" . 1) ("B Y" . 5) ("B Z" . 9)
+    ("C X" . 2) ("C Y" . 6) ("C Z" . 7)))
+
+(defun shapes->score (shapes)
+  (cdr (assoc shapes *shape-scores* :test #'string=)))
+
+(defun outcome->score (outcome)
+  (cdr (assoc outcome *outcome-scores* :test #'string=)))
+
+(defun day2-solve ()
+  (let ((input (read-input "day2" :lines? t)))
+    (values (reduce #'+ (mapcar #'shapes->score input))
+            (reduce #'+ (mapcar #'outcome->score input)))))
